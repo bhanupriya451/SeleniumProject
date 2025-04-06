ff84a1da1d1d96433cffb6cfb7c7a7dbdfe36eb2
bhanupriya <bhanupriya@LAPTOP-8G54P5LT>
2025-04-06 11:27:37 +0530

    first commit

---
+++ JenkinsSample/src/test/java/demo/JenkinsTest.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	@Test
+	public void launch() {
+		WebDriver driver=new ChromeDriver();
+		Reporter.log("Empty chrome browser Launched",true);
+	}
+}
